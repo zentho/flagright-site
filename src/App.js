091834dd043e6475108bea213c0fbb7cb7d3f5a7
Brendan Shih <brenzenitho@gmail.com>
2023-01-23 08:56:36 -0800

    Finish text skeleton for Reliable section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Reliable from "./components/Reliable";
 import Sanctions from "./components/Sanctions";
 
 function App() {
   return (
     <div className="App">
       <Sanctions />
+      <Reliable />
     </div>
   );
 }
---
+++ src/components/Reliable.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Reliable() {
+  return (
+    <section className="p-8 pt-20">
+      <h2 className="mb-4 text-3xl font-semibold">Reliable, global data sources</h2>
+      <p className="leading-7">
+        We support and frequently update our data sources from global lists including OFAC, HM Treasury, UN,
+        and EU to deliver you relevant and up-to-date information.
+      </p>
+    </section>
+  );
+}
