490ca49490de32af67c19695bc53fc7200eb4565
Brendan Shih <brenzenitho@gmail.com>
2023-01-23 08:52:55 -0800

    Finish Sanctions section

--- src/components/Sanctions.js
+++ src/components/Sanctions.js
@@ -4,29 +4,32 @@ import Checkmark from "../media/svg/Checkmark";
 
 export default function Sanctions() {
   return (
-    <section className="p-8 pt-16 ">
-      <h1 className="text-3xl font-semibold mb-6">
-        Sanctions, PEP and media checks on a centralized platform for all AML compliance needs
-      </h1>
-      <div className="flex flex-col gap-6">
-        <div className="flex gap-3 items-start ">
-          <Checkmark />
-          <p className="inline-block max-w-sm leading-7 text-gray-800">
-            Bring all your data on a no-code, powerful platform to maximize the efficacy of AML compliance
-            operations.
-          </p>
+    <section>
+      <div className="p-8 pt-20">
+        <h1 className="text-3xl font-semibold mb-6">
+          Sanctions, PEP and media checks on a centralized platform for all AML compliance needs
+        </h1>
+        <div className="flex flex-col gap-6">
+          <div className="flex gap-3 items-start ">
+            <Checkmark />
+            <p className="inline-block max-w-sm leading-7 text-gray-800">
+              Bring all your data on a no-code, powerful platform to maximize the efficacy of AML compliance
+              operations.
+            </p>
+          </div>
+          <div className="flex gap-3 items-start">
+            <Checkmark />
+            <p className="inline-block max-w-sm leading-7 text-gray-800">
+              Combine screening with transaction monitoring, customer risk assessment and more.
+            </p>
+          </div>
+          <a className="block ml-8 mb-4 text-blue-600 text-lg font-semibold" href="/more">
+            Learn more
+            <Arrow />
+          </a>
         </div>
-        <div className="flex gap-3 items-start">
-          <Checkmark />
-          <p className="inline-block max-w-sm leading-7 text-gray-800">
-            Combine screening with transaction monitoring, customer risk assessment and more.
-          </p>
-        </div>
-        <a className="block ml-8 text-blue-600 text-lg font-medium" href="/more">
-          Learn more
-          <Arrow />
-        </a>
       </div>
+      <img src={require("../media/capone.png")} alt="capone" />
     </section>
   );
 }
