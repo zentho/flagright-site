ff77084183725cdf2cfac202908943a6756fa7f8
Brendan Shih <brenzenitho@gmail.com>
2023-01-23 09:19:15 -0800

    Finish Reliable section

--- src/components/Reliable.js
+++ src/components/Reliable.js
@@ -2,12 +2,18 @@ import React from "react";
 
 export default function Reliable() {
   return (
-    <section className="p-8 pt-20">
+    <section className="p-8 pt-20 pb-24">
       <h2 className="mb-4 text-3xl font-semibold">Reliable, global data sources</h2>
-      <p className="leading-7">
+      <p className="leading-7 mb-8">
         We support and frequently update our data sources from global lists including OFAC, HM Treasury, UN,
         and EU to deliver you relevant and up-to-date information.
       </p>
+      <div className="grid grid-cols-2 items-center w-80 gap-y-10">
+        <img src={require("../media/OFAC.png")} alt="" />
+        <img src={require("../media/FCA.png")} alt="" />
+        <img src={require("../media/EU.png")} alt="" />
+        <img src={require("../media/UN.png")} alt="" />
+      </div>
     </section>
   );
 }
