424499e2be5538f214ee75c01ba40068803dd1cf
Brendan Shih <brenzenitho@gmail.com>
2023-01-24 00:38:26 -0800

    Finish responsiveness for Trust section

--- src/sections/TrustSection.js
+++ src/sections/TrustSection.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function TrustSection() {
   return (
     <section className="px-8 pt-16 pb-12">
-      <div className=" flex flex-col mb-20 text-center">
-        <h2 className="mb-12 text-4xl font-semibold">Our partners</h2>
-        <div className="grid grid-cols-2 gap-x-12 gap-y-8 items-center">
+      <div className="flex flex-col items-center mb-20 text-center">
+        <h2 className="mb-12 text-4xl font-semibold lg:mb-4">Our partners</h2>
+        <div className="grid items-center max-w-sm grid-cols-2 lg:max-w-5xl gap-x-12 gap-y-8 lg:grid-cols-6">
           <img src={require("../media/comply-advantage.png")} alt="comply advantage logo" />
           <img src={require("../media/coinfirm.png")} alt="coinfirm logo" />
           <img className="pr-6" src={require("../media/TRM.png")} alt="TRM logo" />
@@ -14,14 +14,14 @@ export default function TrustSection() {
           <img src={require("../media/rapyd.png")} alt="rapyd logo" />
         </div>
       </div>
-      <div className=" flex flex-col text-center">
+      <div className="flex flex-col items-center text-center">
         <h2 className="mb-3 text-4xl font-semibold">Expert team</h2>
-        <p className="mb-12 leading-7">
+        <p className="max-w-lg mb-12 leading-7 lg:mb-2 lg:max-w-4xl">
           Our team has built anomaly detection & financial crime solutions in the world's most innovative
           companies.
         </p>
-        <div className="grid grid-cols-2 gap-8 gap-x-20 items-center">
-          <img src={require("../media/revolut.png")} alt="revolut logo" />
+        <div className="grid items-center max-w-sm grid-cols-2 gap-8 lg:max-w-2xl gap-x-20 lg:grid-cols-4">
+          <img className="" src={require("../media/revolut.png")} alt="revolut logo" />
           <img src={require("../media/palantir.png")} alt="palantir logo" />
           <img className="p-6" src={require("../media/aws.png")} alt="aws logo" />
           <img src={require("../media/florida-blue.png")} alt="florida blue logo" />
